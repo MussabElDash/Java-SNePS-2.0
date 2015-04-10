8afcf2b911c8c973ffa58b1ff6dbc273fc22e969
Mussab ElDash <mussab14@gmail.com>
2015-04-10 15:13:33 +0200

    commiting

--- pullAll.sh
+++ pullAll.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+function currentBranch()
+{
+	local branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
+	local branch_name="(unnamed branch)"
+	local branch_name=${branch_name##refs/heads/}
+	echo "$branch_name"
+}
+
+branch_name=`currentBranch`
+
+git add .;
+git commit -m "commiting";
+git push origin ${branch_name};
+
 for remote in `git branch -r`; do
 	branch=${remote:7};
 	git branch --track ${branch};
@@ -7,3 +21,5 @@ for remote in `git branch -r`; do
 	git branch --set-upstream-to=origin/${branch} ${branch};
 	git pull origin ${branch}; 
 done
+
+git checkout ${branch_name}

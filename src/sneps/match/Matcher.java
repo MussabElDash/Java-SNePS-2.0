0d72eb8902726b20949a6d4a36189995ca3cb92a
Omar Sherif <osherifo93@gmail.com>
2015-04-13 22:31:45 +0200

    finished termvere-uvbr

--- src/sneps/match/Matcher.java
+++ src/sneps/match/Matcher.java
@@ -1,5 +1,6 @@
 package sneps.match;
 
+import java.util.Enumeration;
 import java.util.LinkedList;
 
 import sneps.DownCable;
@@ -7,102 +8,110 @@ import sneps.DownCableSet;
 import sneps.MolecularNode;
 import sneps.Node;
 import sneps.NodeSet;
+import sneps.PatternNode;
 import sneps.VariableNode;
 
 public class Matcher {
 
 	public boolean hERE(Node sourceNode, Node targetNode,
-			LinkedList<Substitutions> sourceList,LinkedList<Substitutions> targetList,boolean UVBR, boolean rightOrder) {
+			LinkedList<Substitutions> sourceList,
+			LinkedList<Substitutions> targetList, boolean UVBR,
+			boolean rightOrder) {
 		if (!compatible(sourceNode, targetNode))
 			return false;
 		if (sourceNode.getSyntacticType() == "Variable")
-			if (!VARHERE(sourceNode, targetNode, sourceList,targetList,UVBR,rightOrder))
+			if (!VARHERE(sourceNode, targetNode, sourceList, targetList, UVBR,
+					rightOrder))
 				return false;
-			else if( targetNode.getSyntacticType() == "Variable") 
-				if (!VARHERE(sourceNode, targetNode, sourceList,targetList,UVBR,rightOrder))
-					return false;	
-		 else if (sourceNode.getSyntacticType() == "Molecular"
-				&& targetNode.getSyntacticType() == "Molecular")
-		{
-			MolecularNode n1 = (MolecularNode) sourceNode;
-			MolecularNode n2 = (MolecularNode) targetNode;
-			DownCableSet cs1 = n1.getDownCableSet();
-			DownCableSet cs2 = n2.getDownCableSet();
-			if (cs1.getCaseFrame() != cs2.getCaseFrame())
-				return false;
-			else {
-				DownCable[] dcs1 = (DownCable[]) cs1.getDownCables().values()
-						.toArray();
-				DownCable[] dcs2 = (DownCable[]) cs2.getDownCables().values()
-						.toArray();
+			else if (targetNode.getSyntacticType() == "Variable")
+				if (!VARHERE(sourceNode, targetNode, sourceList, targetList,
+						UVBR, rightOrder))
+					return false;
+				else if (sourceNode.getSyntacticType() == "Molecular"
+						&& targetNode.getSyntacticType() == "Molecular") {
+					MolecularNode n1 = (MolecularNode) sourceNode;
+					MolecularNode n2 = (MolecularNode) targetNode;
+					DownCableSet cs1 = n1.getDownCableSet();
+					DownCableSet cs2 = n2.getDownCableSet();
+					if (cs1.getCaseFrame() != cs2.getCaseFrame())
+						return false;
+					else {
+						DownCable[] dcs1 = (DownCable[]) cs1.getDownCables()
+								.values().toArray();
+						DownCable[] dcs2 = (DownCable[]) cs2.getDownCables()
+								.values().toArray();
 
-				for (DownCable cable1 : dcs1)
-					for (DownCable cable2 : dcs2)
-						if (cable1.getRelation().equals(cable2.getRelation())) {
-							int size1 = cable1.getNodeSet().size();
-							int size2 = cable2.getNodeSet().size();
-							String adjust = cable1.getRelation().getAdjust();
+						for (DownCable cable1 : dcs1)
+							for (DownCable cable2 : dcs2)
+								if (cable1.getRelation().equals(
+										cable2.getRelation())) {
+									int size1 = cable1.getNodeSet().size();
+									int size2 = cable2.getNodeSet().size();
+									String adjust = cable1.getRelation()
+											.getAdjust();
 
-							switch (adjust) {
-							case "none":
-								if (size1 != size2)
-									return false;
-							case "reduce":
-								if ((rightOrder && size1 > size2)
-										|| (!rightOrder && size2 > size1))
-									return false;
-							case "expand":
-								if ((rightOrder && size1 < size2)
-										|| (!rightOrder && size2 < size1))
-									return false;
-							}
-							if (!setUnify(cable1.getNodeSet(),
-									cable2.getNodeSet(), sourceList,targetList,UVBR, rightOrder))
-								return false;
-						}
+									switch (adjust) {
+									case "none":
+										if (size1 != size2)
+											return false;
+									case "reduce":
+										if ((rightOrder && size1 > size2)
+												|| (!rightOrder && size2 > size1))
+											return false;
+									case "expand":
+										if ((rightOrder && size1 < size2)
+												|| (!rightOrder && size2 < size1))
+											return false;
+									}
+									if (!setUnify(cable1.getNodeSet(),
+											cable2.getNodeSet(), sourceList,
+											targetList, UVBR, rightOrder))
+										return false;
+								}
 
-			}
+					}
 
-		}
+				}
 
 		return true;
 	}
 
 	public boolean VARHERE(Node sourceNode, Node targetNode,
-			LinkedList<Substitutions> sourcelist,LinkedList<Substitutions> targetList,boolean UVBR,boolean rightOrder) {
+			LinkedList<Substitutions> sourcelist,
+			LinkedList<Substitutions> targetList, boolean UVBR,
+			boolean rightOrder) {
 
 		boolean unifiable = false;
 		LinkedList<Substitutions> variableList;
 		LinkedList<Substitutions> bindingList;
 		VariableNode variableNode;
 		Node bindingNode;
-		if(rightOrder){
-			variableList=sourcelist;
-			bindingList=targetList;
-			variableNode=(VariableNode) sourceNode;
-			bindingNode=targetNode;
+		if (rightOrder) {
+			variableList = sourcelist;
+			bindingList = targetList;
+			variableNode = (VariableNode) sourceNode;
+			bindingNode = targetNode;
+		} else {
+			variableList = targetList;
+			bindingList = variableList;
+			variableNode = (VariableNode) targetNode;
+			bindingNode = sourceNode;
 		}
-		else{
-			variableList=targetList;
-			bindingList=variableList;
-			variableNode=(VariableNode) targetNode;
-			bindingNode=sourceNode;
-		}
-		
+
 		for (int i = 0; i < variableList.size(); i++) {
 			Substitutions currentVSub = variableList.removeFirst();
-            Substitutions currentBSub = bindingList.removeFirst();
+			Substitutions currentBSub = bindingList.removeFirst();
 			if (!currentVSub.isBound(variableNode)) {
-				if(bindingNode.getSyntacticType()=="Variable" && UVBR)
-                        	if(currentBSub.isBound((VariableNode) bindingNode)||currentVSub.isValue(bindingNode))
-				                  continue;
-				                	  
-				
-					currentVSub.putIn(new Binding(variableNode, bindingNode));
-					variableList.add(currentVSub);
-					bindingList.add(currentBSub);
-					unifiable = true;
-				
+				if (bindingNode.getSyntacticType() == "Variable" && UVBR)
+					if (currentBSub.isBound((VariableNode) bindingNode)
+							|| currentVSub.isValue(bindingNode))
+						continue;
+
+				currentVSub.putIn(new Binding(variableNode, bindingNode));
+				variableList.add(currentVSub);
+				bindingList.add(currentBSub);
+				unifiable = true;
+
 			} else {
 				if (currentVSub.getBindingByVariable(variableNode).getNode()
 						.equals(bindingNode)) {
@@ -133,7 +142,8 @@ public class Matcher {
 	}
 
 	public boolean setUnify(NodeSet ns1, NodeSet ns2,
-			LinkedList<Substitutions> sList,LinkedList<Substitutions> tList,boolean UVBR, boolean rightOrder) {
+			LinkedList<Substitutions> sList, LinkedList<Substitutions> tList,
+			boolean UVBR, boolean rightOrder) {
 		if (ns1.size() == 0 || ns2.size() == 0)
 			return true;
 
@@ -156,14 +166,15 @@ public class Matcher {
 					LinkedList<Substitutions> newTList = new LinkedList<Substitutions>();
 					newSList.add(currentSourceSub);
 					newTList.add(currentTargetSub);
-					if (UVBR&&uvbrConflict(ns1, ns2, n1, n2))
+					if (UVBR && uvbrConflict(ns1, ns2, n1, n2))
 						continue;
-						if (hERE(n1, n2, newSList,newTList,UVBR, rightOrder))
-							if (setUnify(others1, others2, newSList,newTList,UVBR, rightOrder)) {
-								sList.addAll(newSList);
-								tList.addAll(newTList);
-								unifiable = true;
-							}
+					if (hERE(n1, n2, newSList, newTList, UVBR, rightOrder))
+						if (setUnify(others1, others2, newSList, newTList,
+								UVBR, rightOrder)) {
+							sList.addAll(newSList);
+							tList.addAll(newTList);
+							unifiable = true;
+						}
 
 				}
 			}
@@ -172,165 +183,216 @@ public class Matcher {
 
 		return unifiable;
 	}
-	
-	public Node vere(VariableNode n,Substitutions sourceR,Substitutions targetR,Substitutions sourceS,Substitutions targetS){
-	Node bindingNode;
-	if(sourceR.isBound(n)){
-		Node RbindingNode=sourceR.term(n);
-		if(RbindingNode.getSyntacticType()=="Base"||RbindingNode.getSyntacticType()=="Closed"){
-	bindingNode=RbindingNode;
-	sourceS.putIn(new Binding(n,RbindingNode));
-	sourceR.update(sourceR.getBindingByVariable(n),n);
 
-	//TODO:add appropriate getters and setters in subs
-	
-		}
-		else if(RbindingNode.getSyntacticType()=="Pattern"){
-/*done*/	sourceR.update(sourceR.getBindingByVariable(n),n);
-/*loop*/    sourceS.update(sourceS.getBindingByVariable(n),n);
-            bindingNode=termVere((MolecularNode) RbindingNode,sourceR,targetR,sourceS,targetS); 
-/*loop*/    sourceS.update(sourceS.getBindingByVariable(n),bindingNode);
-            
-		}
-		else if(sourceS.isBound(n)){
-			if(sourceS.term((VariableNode) n).equals(n))
-/*fail loop*/	return null;
-			else bindingNode=sourceS.term((VariableNode) n);
-			
-		}
-		else
-			bindingNode=sourceR.term(n);
-	}
-	else{
-		sourceR.insert(new Binding(n, n));
-		bindingNode=n;
-		sourceS.insert(new Binding(n,bindingNode));
-	}
-	
-	return bindingNode;
-	}
-	
-	public Node termVere(MolecularNode n,Substitutions sourceR,Substitutions targetR,Substitutions sourceS,Substitutions targetS){
-	MolecularNode changedNode=null;
-	
-	
-	return changedNode;
-	}
-	
+	public Node vere(VariableNode n, Substitutions sourceR,
+			Substitutions targetR, Substitutions sourceS, Substitutions targetS) {
+		Node bindingNode;
+		if (sourceR.isBound(n)) {
+			Node RbindingNode = sourceR.term(n);
+			if (RbindingNode.getSyntacticType() == "Base"
+					|| RbindingNode.getSyntacticType() == "Closed") {
+				bindingNode = RbindingNode;
+				sourceS.putIn(new Binding(n, RbindingNode));
+				sourceR.update(sourceR.getBindingByVariable(n), n);
 
-//	public Node VERE(VariableNode n, Substitutions r, Substitutions s) {
-//
-//		Stack<VariableNode> path = source(n, r);
-//		VariableNode v = path.pop();
-//		Node bindingNode;
-//		if (!r.isBound(v)) {
-//			bindingNode = v;
-//			// done
-//			r.update(r.getBindingByVariable(v), v);
-//		} else if (r.value(v).getSyntacticType() == "Base") {
-//			bindingNode = r.value(v);
-//			// done
-//			r.update(r.getBindingByVariable(v), v);
-//			if (s.isBound(v))
-//				s.update(s.getBindingByVariable(v), bindingNode);
-//			else
-//				s.insert(new Binding(v, bindingNode));
-//		} else if (r.value(v).getSyntacticSuperClass() == "Molecular") {
-//			// done
-//			r.update(r.getBindingByVariable(v), v);
-//			MolecularNode y = (MolecularNode) bindingNode;
-//
-//			// TODO:stack loop start on x
-//
-//			while (!path.isEmpty()) {
-//				/* TODO: s(x)==loop */
-//			}
-//			// stack loop end
-//			bindingNode = termVERE(y, r, s);
-//			if (s.isBound(v))
-//				s.update(s.getBindingByVariable(v), bindingNode);
-//			else
-//				s.insert(new Binding(v, bindingNode));
-//
-//		} else if (!s.isBound(v)) {
-//			bindingNode = v;
-//
-//		} else if (v.Sloop()) {
-//			// fail
-//		} else {
-//			bindingNode = s.value(v);
-//
-//		}
-//
-//		// stack loop for x
-//		while (!path.isEmpty()) {
-//			VariableNode x = path.pop();
-//			if (s.isBound(x))
-//				s.update(s.getBindingByVariable(x), bindingNode);
-//			else
-//				s.insert(new Binding(x, bindingNode));
-//		}
-//		// stack loop end
-//		return bindingNode;
-//	}
-//
-//	public Node termVERE(MolecularNode n, Substitutions r, Substitutions s) {
-//		DownCableSet dcset = n.getDownCableSet();
-//		Hashtable<String, DownCable> dcs = dcset.getDownCables();
-//		String[] dckeys = dcs.keySet().toArray(new String[dcs.size()]);
-//
-//		for (String dckey : dckeys) {
-//			DownCable dc = dcs.get(dckey);
-//			Relation relation = dc.getRelation();
-//			NodeSet ns = dc.getNodeSet();
-//
-//			for (int i = 0; i < ns.size(); i++) {
-//				Node currentNode = ns.getNode(i);
-//
-//				if (currentNode.getSyntacticType() == "Variable") {
-//					if (currentNode.Rdone()) {
-//						if (currentNode.Sloop()) {
-//							// fail
-//						} else {
-//							// replace by s(currentNode)
-//						}
-//					} else if (r.isBound((VariableNode) currentNode)) {
-//						// replace by VERE(currentNode)
-//					}
-//				} else if (currentNode.getSyntacticSuperClass() == "Molecular") {
-//					// replace by termvere(currentNode)
-//				}
-//
-//			}
-//		}
-//
-//		return null;
-//	}
-//
-//	public Stack<VariableNode> source(VariableNode n, Substitutions r) {
-//		// TODO:UVBR Rloop
-//		Stack<VariableNode> path = new Stack<VariableNode>();
-//
-//		VariableNode current = n;
-//		while (r.value(current).getSyntacticType() == "Variable"
-//				&& !r.value(current).equals(n)) {
-//			path.push(current);
-//			current = (VariableNode) r.value(current);
-//
-//		}
-//
-//		return path;
-//	}
+				// TODO:add appropriate getters and setters in subs
 
-	
+			} else if (RbindingNode.getSyntacticType() == "Pattern") {
+				/* done */sourceR.update(sourceR.getBindingByVariable(n), n);
+				/* loop */sourceS.update(sourceS.getBindingByVariable(n), n);
+				bindingNode = termVere((MolecularNode) RbindingNode, sourceR,
+						targetR, sourceS, targetS);
+				if (bindingNode == null)
+					return null;
+				/* loop */sourceS.update(sourceS.getBindingByVariable(n),
+						bindingNode);
+
+			} else if (sourceS.isBound(n)) {
+				if (sourceS.term((VariableNode) n).equals(n))
+					/* fail loop */return null;
+				else
+					bindingNode = sourceS.term((VariableNode) n);
+
+			} else
+				bindingNode = sourceR.term(n);
+		} else {
+			sourceR.insert(new Binding(n, n));
+			bindingNode = n;
+			sourceS.insert(new Binding(n, bindingNode));
+		}
+
+		return bindingNode;
+	}
+
+	public MolecularNode termVere(MolecularNode oldNode, Substitutions sourceR,
+			Substitutions targetR, Substitutions sourceS, Substitutions targetS) {
+		MolecularNode newNode = null;
+		DownCableSet newDCS = null;
+		DownCableSet oldDCS = oldNode.getDownCableSet();
+		Enumeration<DownCable> oldDCs = oldDCS.getDownCables().elements();
+		LinkedList<DownCable> newDClist = new LinkedList<DownCable>();
+		while (oldDCs.hasMoreElements()) {
+			DownCable currentOldCable = oldDCs.nextElement();
+			NodeSet currentOldNS = currentOldCable.getNodeSet();
+			NodeSet currentNewNS = new NodeSet();
+			for (int i = 0; i < currentOldNS.size(); i++) {
+				Node currentOldNode = currentOldNS.getNode(i);
+				Node currentNewNode = null;
+
+				if (currentOldNode.getSyntacticType() == "Variable") {
+					if (targetR.isBound((VariableNode) currentOldNode)) {
+						if (targetR.term((VariableNode) currentOldNode) == currentOldNode) {
+							if (targetS.term((VariableNode) currentOldNode) == currentOldNode)
+								return null;
+							else
+								currentNewNode = targetS
+										.term((VariableNode) currentOldNode);
+						} else
+							currentNewNode = vere(
+									(VariableNode) currentOldNode, targetR,
+									sourceR, targetS, sourceS);
+
+					}
+
+					else
+						currentNewNode = currentOldNode;
+
+				} else if (currentOldNode.getSyntacticType() == "Pattern")
+					currentNewNode = termVere((PatternNode) currentOldNode,
+							targetR, sourceR, targetS, sourceS);
+
+				else
+					currentNewNode = currentOldNode;
+
+				currentNewNS.addNode(currentNewNode);
+			}
+			newDClist.add(new DownCable(currentOldCable.getRelation(),
+					currentNewNS));
+		}
+
+		newDCS = new DownCableSet(newDClist, oldDCS.getCaseFrame());
+		try {
+			newNode = new MolecularNode(oldNode.getSyntacticType(),
+			// TODO:name
+					oldNode.getSemanticType(), "tempName", newDCS);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		return newNode;
+	}
+
+	// public Node VERE(VariableNode n, Substitutions r, Substitutions s) {
+	//
+	// Stack<VariableNode> path = source(n, r);
+	// VariableNode v = path.pop();
+	// Node bindingNode;
+	// if (!r.isBound(v)) {
+	// bindingNode = v;
+	// // done
+	// r.update(r.getBindingByVariable(v), v);
+	// } else if (r.value(v).getSyntacticType() == "Base") {
+	// bindingNode = r.value(v);
+	// // done
+	// r.update(r.getBindingByVariable(v), v);
+	// if (s.isBound(v))
+	// s.update(s.getBindingByVariable(v), bindingNode);
+	// else
+	// s.insert(new Binding(v, bindingNode));
+	// } else if (r.value(v).getSyntacticSuperClass() == "Molecular") {
+	// // done
+	// r.update(r.getBindingByVariable(v), v);
+	// MolecularNode y = (MolecularNode) bindingNode;
+	//
+	// // TODO:stack loop start on x
+	//
+	// while (!path.isEmpty()) {
+	// /* TODO: s(x)==loop */
+	// }
+	// // stack loop end
+	// bindingNode = termVERE(y, r, s);
+	// if (s.isBound(v))
+	// s.update(s.getBindingByVariable(v), bindingNode);
+	// else
+	// s.insert(new Binding(v, bindingNode));
+	//
+	// } else if (!s.isBound(v)) {
+	// bindingNode = v;
+	//
+	// } else if (v.Sloop()) {
+	// // fail
+	// } else {
+	// bindingNode = s.value(v);
+	//
+	// }
+	//
+	// // stack loop for x
+	// while (!path.isEmpty()) {
+	// VariableNode x = path.pop();
+	// if (s.isBound(x))
+	// s.update(s.getBindingByVariable(x), bindingNode);
+	// else
+	// s.insert(new Binding(x, bindingNode));
+	// }
+	// // stack loop end
+	// return bindingNode;
+	// }
+	//
+	// public Node termVERE(MolecularNode n, Substitutions r, Substitutions s) {
+	// DownCableSet dcset = n.getDownCableSet();
+	// Hashtable<String, DownCable> dcs = dcset.getDownCables();
+	// String[] dckeys = dcs.keySet().toArray(new String[dcs.size()]);
+	//
+	// for (String dckey : dckeys) {
+	// DownCable dc = dcs.get(dckey);
+	// Relation relation = dc.getRelation();
+	// NodeSet ns = dc.getNodeSet();
+	//
+	// for (int i = 0; i < ns.size(); i++) {
+	// Node currentNode = ns.getNode(i);
+	//
+	// if (currentNode.getSyntacticType() == "Variable") {
+	// if (currentNode.Rdone()) {
+	// if (currentNode.Sloop()) {
+	// // fail
+	// } else {
+	// // replace by s(currentNode)
+	// }
+	// } else if (r.isBound((VariableNode) currentNode)) {
+	// // replace by VERE(currentNode)
+	// }
+	// } else if (currentNode.getSyntacticSuperClass() == "Molecular") {
+	// // replace by termvere(currentNode)
+	// }
+	//
+	// }
+	// }
+	//
+	// return null;
+	// }
+	//
+	// public Stack<VariableNode> source(VariableNode n, Substitutions r) {
+	// // TODO:UVBR Rloop
+	// Stack<VariableNode> path = new Stack<VariableNode>();
+	//
+	// VariableNode current = n;
+	// while (r.value(current).getSyntacticType() == "Variable"
+	// && !r.value(current).equals(n)) {
+	// path.push(current);
+	// current = (VariableNode) r.value(current);
+	//
+	// }
+	//
+	// return path;
+	// }
+
 	public boolean uvbrConflict(NodeSet ns1, NodeSet ns2, Node n, Node m) {
 		return (n.getSyntacticType() == "Variable" || m.getSyntacticType() == "Variable")
 				&& (ns1.contains(m) || ns2.contains(n));
 	}
-	
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+
 	}
-	
+
 }

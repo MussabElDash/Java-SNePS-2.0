d55a2ba372eef4648a9ac7f6d42f2e2bcabaeeab
Mussab ElDash <mussab14@gmail.com>
2015-05-13 13:07:50 +0300

    add Tests to PTreeSIndex and to RuisSIndex

--- src/snip/Rules/Tests/PTreeSindexTest.java
+++ src/snip/Rules/Tests/PTreeSindexTest.java
@@ -27,20 +27,22 @@ import snip.Rules.DataStructures.SIndex;
 import snip.Rules.Interfaces.NodeWithVar;
 import SNeBR.Support;
 
-public class SingletonSIndexTest {
+public class PTreeSindexTest {
 	public static void main(String[] args) throws Exception {
 		CaseFrame.createRuleCaseFrame();
-		Relation[] relations = new Relation[9];
-		for (int i = 0; i < relations.length; i += 3) {
-			relations[i] = Network.defineRelation((char) ('A' + i / 3) + "1",
+		Relation[] relations = new Relation[12];
+		for (int i = 0; i < relations.length; i += 4) {
+			relations[i] = Network.defineRelation((char) ('A' + i / 4) + "1",
 					"Infimum", "none", 1);
-			relations[i + 1] = Network.defineRelation((char) ('A' + i / 3)
+			relations[i + 1] = Network.defineRelation((char) ('A' + i / 4)
 					+ "2", "Infimum", "none", 1);
-			relations[i + 2] = Network.defineRelation((char) ('A' + i / 3)
+			relations[i + 2] = Network.defineRelation((char) ('A' + i / 4)
 					+ "3", "Infimum", "none", 1);
+			relations[i + 3] = Network.defineRelation((char) ('A' + i / 4)
+					+ "4", "Infimum", "none", 1);
 		}
 
-		VariableNode[] varNodes = getVarNodes(3);
+		VariableNode[] varNodes = getVarNodes(5);
 
 		ArrayList<LinkedList<RCFP>> relationsProperties = getRelProp(relations);
 
@@ -58,27 +60,29 @@ public class SingletonSIndexTest {
 			nodeSet.addNode((Node) mn);
 
 		HashSet<Integer> varsIds = new HashSet<>();
-		for (VariableNode varNode : varNodes)
-			varsIds.add(varNode.getId());
+		varsIds.add(varNodes[0].getId());
+		varsIds.add(varNodes[1].getId());
 
-		SIndex indexing = new SIndex(null, varsIds, SIndex.SINGLETONRUIS,
-				nodeSet);
+		SIndex indexing = new SIndex(null, varsIds, SIndex.PTREE, nodeSet);
 
 		Node bob = Network.buildBaseNode("Bob", new Individual());
 		Node mary = Network.buildBaseNode("mary", new Individual());
+		Node tweety = Network.buildBaseNode("tweety", new Individual());
 		Node mussab = Network.buildBaseNode("mussab", new Individual());
 		Node eldash = Network.buildBaseNode("eldash", new Individual());
 
 		System.out.println("====================================");
 		System.out.println("Inserting a RUIS in the SIndex");
-		insertIntoSIndexing(indexing, bob, mary, mussab, antNodesTemp, varNodes);
+		insertIntoSIndexing(indexing, bob, mary, tweety, mussab, eldash,
+				antNodesTemp, varNodes);
 
 		System.out.println("Inserting another RUIS in the SIndex");
-		insertIntoSIndexing(indexing, mussab, bob, mary, antNodesTemp, varNodes);
+		insertIntoSIndexing(indexing, mussab, bob, tweety, eldash, mary,
+				antNodesTemp, varNodes);
 
 		System.out.println("Inserting yet another RUIS in the SIndex");
-		insertIntoSIndexing(indexing, mussab, bob, eldash, antNodesTemp,
-				varNodes);
+		insertIntoSIndexing(indexing, mussab, bob, eldash, mary, tweety,
+				antNodesTemp, varNodes);
 	}
 
 	private static VariableNode[] getVarNodes(int n) {
@@ -92,17 +96,20 @@ public class SingletonSIndexTest {
 
 	private static ArrayList<LinkedList<RCFP>> getRelProp(Relation[] relations) {
 		ArrayList<LinkedList<RCFP>> res = new ArrayList<LinkedList<RCFP>>();
-		for (int i = 0; i < relations.length; i += 3) {
+		for (int i = 0; i < relations.length; i += 4) {
 			RCFP rel1 = Network.defineRelationPropertiesForCF(relations[i],
 					"none", 1);
 			RCFP rel2 = Network.defineRelationPropertiesForCF(relations[i + 1],
 					"none", 1);
 			RCFP rel3 = Network.defineRelationPropertiesForCF(relations[i + 2],
 					"none", 1);
+			RCFP rel4 = Network.defineRelationPropertiesForCF(relations[i + 3],
+					"none", 1);
 			LinkedList<RCFP> relCF1 = new LinkedList<RCFP>();
 			relCF1.add(rel1);
 			relCF1.add(rel2);
 			relCF1.add(rel3);
+			relCF1.add(rel4);
 			res.add(relCF1);
 		}
 		return res;
@@ -121,36 +128,39 @@ public class SingletonSIndexTest {
 	private static NodeWithVar[] buildNodes(VariableNode[] varNodes,
 			Relation[] relations, CaseFrame[] caseFrames) throws Exception {
 		NodeWithVar[] res = new NodeWithVar[(int) Math
-				.ceil(relations.length / 3)];
+				.ceil(relations.length / 4)];
 		res[0] = (NodeWithVar) buildMolecularNode(varNodes[0], varNodes[1],
-				varNodes[2], relations[0], relations[1], relations[2],
-				caseFrames[0]);
+				varNodes[2], varNodes[3], relations[0], relations[1],
+				relations[2], relations[3], caseFrames[0]);
 		res[1] = (NodeWithVar) buildMolecularNode(varNodes[0], varNodes[1],
-				varNodes[2], relations[3], relations[4], relations[5],
-				caseFrames[1]);
+				varNodes[2], varNodes[4], relations[4], relations[5],
+				relations[6], relations[7], caseFrames[1]);
 		res[2] = (NodeWithVar) buildMolecularNode(varNodes[0], varNodes[1],
-				varNodes[2], relations[6], relations[7], relations[8],
-				caseFrames[2]);
+				varNodes[3], varNodes[4], relations[8], relations[9],
+				relations[10], relations[11], caseFrames[2]);
 		return res;
 	}
 
 	private static MolecularNode buildMolecularNode(VariableNode variableNode,
 			VariableNode variableNode2, VariableNode variableNode3,
-			Relation relation, Relation relation2, Relation relation3,
-			CaseFrame caseFrame) throws Exception {
-		Object[][] a1 = new Object[3][2];
+			VariableNode variableNode4, Relation relation, Relation relation2,
+			Relation relation3, Relation relation4, CaseFrame caseFrame)
+			throws Exception {
+		Object[][] a1 = new Object[4][2];
 		a1[0][0] = relation;
 		a1[0][1] = variableNode;
 		a1[1][0] = relation2;
 		a1[1][1] = variableNode2;
 		a1[2][0] = relation3;
 		a1[2][1] = variableNode3;
+		a1[3][0] = relation4;
+		a1[3][1] = variableNode4;
 		return Network.buildMolecularNode(a1, caseFrame);
 	}
 
-	private static RuleUseInfo getPosRui(Node signature, VariableNode var1,
-			VariableNode var2, VariableNode var3, Node sub1, Node sub2,
-			Node sub3) {
+	private static RuleUseInfo getRui(Node signature, VariableNode var1,
+			VariableNode var2, VariableNode var3, VariableNode var4, Node sub1,
+			Node sub2, Node sub3, Node sub4) {
 		FlagNode fn = new FlagNode(signature, new Support(), 1);
 		FlagNodeSet fns = new FlagNodeSet();
 		fns.insert(fn);
@@ -158,36 +168,24 @@ public class SingletonSIndexTest {
 		sub = sub.insert(new Binding(var1, sub1));
 		sub = sub.insert(new Binding(var2, sub2));
 		sub = sub.insert(new Binding(var3, sub3));
+		sub = sub.insert(new Binding(var4, sub4));
 		RuleUseInfo rui = new RuleUseInfo(sub, 1, 0, fns);
 		return rui;
 	}
 
-	private static RuleUseInfo getNegRui(Node signature, VariableNode var1,
-			VariableNode var2, VariableNode var3, Node sub1, Node sub2,
-			Node sub3) {
-		FlagNode fn = new FlagNode(signature, new Support(), 2);
-		FlagNodeSet fns = new FlagNodeSet();
-		fns.insert(fn);
-		Substitutions sub = new LinearSubstitutions();
-		sub = sub.insert(new Binding(var1, sub1));
-		sub = sub.insert(new Binding(var2, sub2));
-		sub = sub.insert(new Binding(var3, sub3));
-		RuleUseInfo rui = new RuleUseInfo(sub, 0, 1, fns);
-		return rui;
-	}
-
 	private static void insertIntoSIndexing(SIndex sindexing, Node c1, Node c2,
-			Node c3, NodeWithVar[] antNodesTemp, VariableNode[] varNodes) {
-		RuleUseInfo rui = getPosRui((Node) antNodesTemp[0], varNodes[0],
-				varNodes[1], varNodes[2], c1, c2, c3);
+			Node c3, Node c4, Node c5, NodeWithVar[] antNodesTemp,
+			VariableNode[] varNodes) {
+		RuleUseInfo rui = getRui((Node) antNodesTemp[0], varNodes[0],
+				varNodes[1], varNodes[2], varNodes[3], c1, c2, c3, c4);
 		printRuis(sindexing.insertRUI(rui));
 
-		rui = getNegRui((Node) antNodesTemp[1], varNodes[0], varNodes[1],
-				varNodes[2], c1, c2, c3);
+		rui = getRui((Node) antNodesTemp[1], varNodes[0], varNodes[1],
+				varNodes[2], varNodes[4], c1, c2, c3, c5);
 		printRuis(sindexing.insertRUI(rui));
 
-		rui = getPosRui((Node) antNodesTemp[2], varNodes[0], varNodes[1],
-				varNodes[2], c1, c2, c3);
+		rui = getRui((Node) antNodesTemp[2], varNodes[0], varNodes[1],
+				varNodes[3], varNodes[4], c1, c2, c4, c5);
 		printRuis(sindexing.insertRUI(rui));
 	}
 
